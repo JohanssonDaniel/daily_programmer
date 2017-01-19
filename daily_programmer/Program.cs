466e85bf5f60c976c8d087263efa297f917bf842
JohanssonDaniel <johanssondaniel93@gmail.com>
2017-01-19 17:15:57 +0900

    Separated individual solutions from master

--- daily_programmer/Program.cs
+++ daily_programmer/Program.cs
@@ -10,45 +10,7 @@ namespace daily_programmer
     {
         static void Main(string[] args)
         {
-            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ".ToCharArray();
-
-            Console.WriteLine("This program will take take two strings and change each individual");
-            Console.WriteLine("letter in the first string until it is the same as the second string");
-
-            Console.Write("Input first string: ");
-            string firstString = Console.ReadLine();
-
-            Console.Write("Input second string(same length): ");
-            string secondString = Console.ReadLine();
-
-            if(firstString.Length == secondString.Length)
-            { 
-                int size = firstString.Length;
-                int alphaSize = alpha.Length;
-
-                char[] firstChar = firstString.ToUpper().ToCharArray();
-                char[] secondChar = secondString.ToUpper().ToCharArray();
-
-                for (int i = 0; i < size; i++)
-                {
-                    for (int a = 0; a < alphaSize; a++)
-                    { 
-                        if (firstChar[i] == secondChar[i])
-                        {
-                            Console.WriteLine(firstChar);
-                            break;
-                        }
-                        else
-                        {
-                            firstChar[i] = alpha[a];
-                        }
-                    }
-                }
-                    
-                firstString = new string(firstChar).ToLower();
-                Console.WriteLine(firstString);
-                Console.ReadLine();
-            }
+            LetterByLetter.Algorithm();
         }
     }
 }
